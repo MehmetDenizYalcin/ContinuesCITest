5326df42a829d51d5e616c7f4a63302942aa210e
Mehmet Deniz Yalcin <33126416+MehmetDenizYalcin@users.noreply.github.com>
2020-02-08 19:53:40 +0200

    first commit

---
+++ ContinuesCITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContinuesCITest
+//
+//  Created by Mehmet Deniz Yalcin on 8.02.2020.
+//  Copyright © 2020 Mehmet Deniz Yalcin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ContinuesCITestTests/ContinuesCITestTests.swift
@@ -0,0 +1,34 @@
+//
+//  ContinuesCITestTests.swift
+//  ContinuesCITestTests
+//
+//  Created by Mehmet Deniz Yalcin on 8.02.2020.
+//  Copyright © 2020 Mehmet Deniz Yalcin. All rights reserved.
+//
+
+import XCTest
+@testable import ContinuesCITest
+
+class ContinuesCITestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
